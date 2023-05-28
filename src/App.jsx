04e78ffa2987a281af2ed19b47b92fd110e626b1
Ilai Chibotaro <user@Users-MacBook-Pro.local>
2023-05-28 19:49:24 +0300

    Changed state logic to array of x and o values

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
+import { useState } from "react";
+
 import "./App.css";
-import { Button } from "./Components/Button";
 
 function App() {
+  const [turn, setTurn] = useState('x')
+  const [board, setBoard] = useState(Array(9).fill(undefined))
+  
+  function buttonOnClick(event) {
+    handelClick(val.target.value)
+  }
+
+  function handelClick(buttonClicked) {
+    board[buttonClicked-1] = turn
+    setBoard(board)
+
+    if (turn == 'x') {
+      return setTurn('o')
+    } else {
+      return setTurn('x')
+    }
+  }
+
+
   return (
     <>
-      <div className="board-game ">
-        <Button>x</Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+      <div className="board-game">
+        <button value={1} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[0]}>{board[0]}</button>
+        <button value={2} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[1]}>{board[1]}</button>
+        <button value={3} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[2]}>{board[2]}</button>
+        <button value={4} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[3]}>{board[3]}</button>
+        <button value={5} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[4]}>{board[4]}</button>
+        <button value={6} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[5]}>{board[5]}</button>
+        <button value={7} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[6]}>{board[6]}</button>
+        <button value={8} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[7]}>{board[7]}</button>
+        <button value={9} onClick={(val)=>{handelClick(val.target.value)}} disabled={board[8]}>{board[8]}</button>
       </div>
     </>
   );
