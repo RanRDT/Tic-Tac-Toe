e47016aa5dcb37fe7c67c8a064c5b6afee28d8f1
RanRDT <129321063+RanRDT@users.noreply.github.com>
2023-05-28 11:58:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import {Button} from "./Components/Button";
+function App() {
+  
+
+  return (
+    <>
+      <div className='bord-game'>
+        <div className='row1'>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        </div>
+        <div className='row2' > 
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        </div>
+        <div className='row3' >
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
